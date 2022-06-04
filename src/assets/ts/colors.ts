598e3f0bc144405938e32f4392f3eee7256fd049
Israel <israelpalomoe@gmail.com>
2022-06-04 12:04:13 +0200

    build():add file colors.ts

---
+++ src/assets/ts/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  purple: '#563d6e',
+  green: '#3DC26A',
+  bg: '#E7EAEE',
+  blue: '#5ECBDC',
+  purple__light: '#826A96',
+  orange: '#E98D7C',
+  white: '#F5F5F5',
+  black: '#4D5052',
+};
